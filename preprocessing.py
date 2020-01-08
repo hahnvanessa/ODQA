fe9d0c5df43356a552f23a53c0769752e5aff83d
Fabian Gössl <s8fagoes@stud.uni-saarland.de>
2020-01-08 14:39:08 +0100

    Quasart function added.

--- preprocessing.py
+++ preprocessing.py
@@ -15,22 +15,37 @@ def process_quasar(folder, set_type, doc_size):
     with open(question_file_path, "r") as qf:
         # Parse each line separate to avoid memory issues
         for line in qf:
-            parsed_line = json.loads(line)
-            question = parsed_line["question"]
-            question_id = parsed_line["uid"]
-            question_answer = parsed_line["answer"]
+            parsed_question = json.loads(line)
+            question = parsed_question["question"]
+            question_id = parsed_question["uid"]
+            question_answer = parsed_question["answer"]
             question_dic[question_id] = {"question":question, "answer":question_answer, "contexts":[]}
 
+    # Check whether the question dictionary has key-value pairs
+    assert len(question_dic) > 0, "question dictionary is empty"
 
     # Contexts File and Path
     context_file = set_type + "_contexts.json"
     context_file_path = "\\".join([folder, "contexts", doc_size, context_file, context_file])
     print(context_file_path)
-    with open(context_file_path, "r") as qf:
-        pass
-        # todo: throw an exception if there is no question for a found answer, i.e. the uid is not matching
+    with open(context_file_path, "r") as cf:
+        for line in cf:
+            parsed_answer = json.loads(line)
+            # Answer ID should have a corresponding question ID
+            answer_id = parsed_answer["uid"]
+            # List of contexts with retrieval scores, contexts are sorted from highest to lowest score
+            answer_contexts = parsed_answer["contexts"]
+            question_dic[answer_id]["contexts"] = answer_contexts
 
-    pass
+        # todo: determine whether it is computationally more efficient to save a list of tuplles instead of a nested list
+        # todo: throw an exception if there is no question for a found answer, i.e. the uid is not matching
+        # todo: check if the encoding of the contexts is correct, think I saw a "u355" wrongly encoded piece
+
+        # Check if every question has contexts
+        for qid, q in question_dic.items():
+            assert len(q["contexts"])>0, "Question {} missing context".format(qid)
+
+        return question_dic
 
 def main(type, folder, set_type, doc_size):
     '''
@@ -46,6 +61,7 @@ def main(type, folder, set_type, doc_size):
     elif type == "searchqa":
         return process_searchqa()
     else:
+        # A wrong type should be identified by argparse already but this is another safeguard
         return ValueError("type must be either 'quasar' or 'searchqa'")
 
     # Return None for now
