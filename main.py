a80685fd34c8b1333a7f276fc2a62d8f2ec9d15b
“Katya <katjasaveleva@gmail.com>
2020-02-13 13:58:19 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser
-import BILSTM
+from BILSTM import BiLSTM, attention
+import os
 import pickle
-import embeddings
-
+from torch import nn
+import torch
 
 def get_file_paths(data_dir):
     # Get paths for all files in the given directory
@@ -25,33 +26,43 @@ def main(embedding_matrix, encoded_corpora):
     :return:
     '''
 
+    embedding_matrix = pickle.load(open(embedding_matrix, 'rb'))
+
     # Create BILSTMs
-    qp_bilstm = BILSTM(embedding_matrix, embedding_dim=300, hidden_dim=100,
-                batch_size=30)
-    interaction_bilstm = BILSTM(embedding_matrix, embedding_dim=300, hidden_dim=100,
-                batch_size=30)
+    qp_bilstm = BiLSTM(embedding_matrix, embedding_dim=300, hidden_dim=100,
+                batch_size=1)
+    interaction_bilstm = BiLSTM(embedding_matrix, embedding_dim=400, hidden_dim=100,
+                batch_size=1)
+    G_bilstm = nn.LSTM(input_size=400, hidden_size=100, bidirectional=True)
 
     # Retrieve the filepaths of all encoded corpora
     file_paths = get_file_paths(encoded_corpora)
 
     qp_representations = {}
+    int_representations = {}
 
     for file in file_paths:
-        with open(os.path.join(file, 'r')) as f:
-            content = pickle.load(f, 'rb')
+        with open(os.path.join(file), 'rb') as f:
+            content = pickle.load(f)
             for item in content:
-                item_id = item.key()
-                question = item[item_id]['question']
+                item_id = item
+                question = content[item_id]['encoded_question']
+                question = torch.tensor(question).to(torch.int64)
                 q_representation = qp_bilstm.forward(question)  # get the question representation
-                contexts = item[item_id]['contexts']
+                contexts = content[item_id]['encoded_contexts']
                 c_representations = []
+                G_ps = []
                 for context in contexts:
+                    context = torch.tensor(context).to(torch.int64)
                     c_representation = qp_bilstm.forward(context)  # get the context representation
                     c_representations.append(c_representation)
+                    HP_attention = attention(q_representation, c_representation)
+                    G_input = torch.cat((c_representation, HP_attention), 2)
+                    G_p = G_bilstm.forward(G_input)
+                    G_ps.append(G_p)
+                int_representations[item_id] = G_ps
                 qp_representations[item_id] = {'q_repr': q_representation,
                                                'c_repr': c_representations}
-            # TODO: get question and context interaction
-
 
 if __name__ == '__main__':
 
@@ -66,4 +77,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Call main()
-    main(embedding_matrix=pickle.load(args.embeddings, 'rb'), encoded_corpora=args.data)
+    main(embedding_matrix=args.embeddings, encoded_corpora=args.data)
