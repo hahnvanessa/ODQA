b0a056c8026e43fd8e791ebcbfb26a2652413601
Fabian Gössl <s8fagoes@stud.uni-saarland.de>
2020-02-19 18:14:21 +0100

    Added function batch_training

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import os
 import pickle
 from torch import nn
 import torch
+from torch.nn.utils.rnn import pack_padded_sequence
+import question_answer_set
+from torch.utils.data import Dataset, DataLoader
+
+MAX_SEQUENCE_LENGTH = 100
 
 def get_file_paths(data_dir):
     # Get paths for all files in the given directory
-
     file_names = []
     # r=root, d=directories, f = files
     for r, d, f in os.walk(args.data):
@@ -15,6 +19,53 @@ def get_file_paths(data_dir):
             if '.pkl' in file:
                 file_names.append(os.path.join(r, file))
     return file_names
+
+
+
+def batch_training(dataset, embedding_matrix, batch_size=6, num_epochs=10):
+    '''
+    Performs minibatch training
+    :param dataset:
+    :param embedding_matrix:
+    :param batch_size:
+    :param num_epochs:
+    :return:
+    '''
+    # Load Dataset with the dataloader
+    train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+
+    # Store representations
+    qp_representations = {}
+    int_representations = {}
+
+    # Initialize BiLSTMs
+    qp_bilstm = BiLSTM(embedding_matrix, embedding_dim=300, hidden_dim=100,
+                batch_size=1)
+    interaction_bilstm = BiLSTM(embedding_matrix, embedding_dim=400, hidden_dim=100,
+                batch_size=1)
+    G_bilstm = nn.LSTM(input_size=400, hidden_size=100, bidirectional=True)
+
+
+    for epoch in range(num_epochs):
+
+        for batch_number, data in enumerate(train_loader):
+            questions, contexts, answers, q_len, c_len, a_len = data
+            # Pack (reduce the sentences to their original form without padding)
+            packed_q = torch.nn.utils.rnn.pack_padded_sequence(questions, q_len, batch_first=True)
+            packed_c = torch.nn.utils.rnn.pack_padded_sequence(contexts, c_len, batch_first=True)
+            packed_a = torch.nn.utils.rnn.pack_padded_sequence(answers, a_len, batch_first=True)
+            # Forward operations
+            q_representation = qp_bilstm.forward(packed_q)
+            c_representation = qp_bilstm.forward(packed_c)
+            HP_attention = attention(q_representation, c_representation)
+            # todo: finish the forward operations, this is basically just transferring
+            # stuff from the main function into here
+
+            # Unpack (add the paddings)
+            G_p_packed = torch.nn.utils.rnn.pad_packed_sequence(G_p_packed, batch_first=True)
+            G_ps.append(G_p_packed)
+
+
 
 
 def main(embedding_matrix, encoded_corpora):
@@ -44,6 +95,11 @@ def main(embedding_matrix, encoded_corpora):
     for file in file_paths:
         with open(os.path.join(file), 'rb') as f:
             content = pickle.load(f)
+
+            # Minibatch training
+            dataset = question_answer_set(content)
+            batch_training(dataset, embedding_matrix, batch_size=6, num_epochs=10)
+
             for item in content:
                 item_id = item
                 question = content[item_id]['encoded_question']
@@ -52,6 +108,11 @@ def main(embedding_matrix, encoded_corpora):
                 contexts = content[item_id]['encoded_contexts']
                 c_representations = []
                 G_ps = []
+
+                # Packing, requires padded,embedded sequences
+                # todo: check if we need to embed numbers with <N>
+                x_packed = pack_padded_sequence(x_embed, x_lens, batch_first=True, enforce_sorted=False)
+
                 for context in contexts:
                     context = torch.tensor(context).to(torch.int64)
                     c_representation = qp_bilstm.forward(context)  # get the context representation
