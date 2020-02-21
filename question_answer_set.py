27722a30ba4e471f8d59e264c7aca20f4a926f39
Fabian Gössl <s8fagoes@stud.uni-saarland.de>
2020-02-21 11:35:56 +0100

    Optimized data storing

--- question_answer_set.py
+++ question_answer_set.py
@@ -9,23 +9,16 @@ class question_answer_set(Dataset):
     create minibatches.
     '''
 
-
     def __init__(self, file_content):
         # todo: implement a max size parameter to allow for mini-sets, it should stop reading in content when
-        # reaching max_size
-        # Questions
-        self.questions = None
-        self.question_lengths = None
-        # Contexts
-        self.contexts = None
-        self.context_lengths = None
-        # Answers
-        self.answers = None
-        self.answer_lengths = None
-        # Type of Tensor
-        self.longtype = torch.int64
-        # Length of dataset (number of question_context_answer pairs)
         self.set_len = None
+
+        # Tensor storage
+        self.questionid_context_answerid = []
+        self.questions = {}
+        self.answers = {}
+
+        # Read in Dataset
         self.read_in_dataset(file_content)
 
 
@@ -34,43 +27,36 @@ class question_answer_set(Dataset):
         :param
         :return:
         '''
-        question_context_answer_pairs = []
         self.set_len = None
 
         # Read q,c,a and convert to a torch tensor
         # todo: ensure that this torch tensor is of self.LongType
         # todo: we could make this more memory efficient if we would store a mapping to the question
         # insteado of the question itself for every q,c,a pair
-        for item in content:
+        for idx, item in enumerate(content):
             item_id = item
             question = content[item_id]['encoded_question']
             contexts = content[item_id]['encoded_contexts']
             answer = content[item_id]['encoded_answer']
+            # Store questions, contexts and answers as tensors
             for context in contexts:
-                question_context_answer_pairs.append([question, context, answer])
+                self.questionid_context_answerid.append((idx, torch.from_numpy(context).type(torch.LongTensor), idx))
+            self.questions[idx] = torch.from_numpy(question).type(torch.LongTensor)
+            self.answers[idx] = torch.from_numpy(answer).type(torch.LongTensor)
+        self.set_len = len(self.questionid_context_answerid)
 
-        self.set_len = len(question_context_answer_pairs)
-        # Initialize empty matrices
-        self.questions = torch.zeros(self.set_len, MAX_SEQUENCE_LENGTH,dtype=self.longtype)
-        self.question_lengths = torch.zeros(self.set_len,1,dtype=self.longtype)
-        self.contexts = torch.zeros(self.set_len, MAX_SEQUENCE_LENGTH,dtype=self.longtype)
-        self.context_lengths = torch.zeros(self.set_len,1,dtype=self.longtype)
-        self.answers = torch.zeros(self.set_len, MAX_SEQUENCE_LENGTH,dtype=self.longtype)
-        self.answer_lengths = torch.zeros(self.set_len, 1, dtype=self.longtype)
+    def get_question(self, index):
+        return self.questions[self.questionid_context_answerid[index][0]]
 
-        # Update the matrices
-        # note: inefficient, stores questions and answers multiple times
-        for idx, (q, c, a) in enumerate(question_context_answer_pairs):
-            # Update embedding matrices
-            self.questions[idx] = torch.from_numpy(q)
-            self.contexts[idx] = torch.from_numpy(c)
-            self.answers[idx] = torch.from_numpy(a)
+    def get_answer(self, index):
+        return self.answers[self.questionid_context_answerid[index][2]]
 
-            # Update length vectors
-            self.question_lengths[idx] = len(q[q.nonzero()])
-            self.context_lengths[idx] = len(c[c.nonzero()])
-            self.answer_lengths[idx] = len(a[a.nonzero()])
-        print("Dataset is now ready for action.") #todo: delete
+    def get_context(self, index):
+        return self.questionid_context_answerid[index][1]
+
+    def determine_length(self, tensor):
+        ''' Determines length without padding '''
+        return len(tensor[tensor.nonzero()])
 
     def __getitem__(self, index):
         '''
@@ -79,8 +65,10 @@ class question_answer_set(Dataset):
         :param index:
         :return:
         '''
-        # todo: could we calculate the lengths on the fly within getitem?
-        return self.questions[index], self.contexts[index], self.answers[index], self.question_lengths[index], self.context_lengths[index], self.answer_lengths[index]
+        question = self.get_question(index)
+        context = self.get_context(index)
+        answer = self.get_answer(index)
+        return question, context, answer, self.determine_length(question), self.determine_length(context), self.determine_length(answer)
 
 
     def __len__(self):
