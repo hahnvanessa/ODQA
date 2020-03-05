b6a81f8ca8b5b6dc3f75bfc2e3ffb120780e8d19
Fabian Gössl <s8fagoes@stud.uni-saarland.de>
2020-03-05 17:09:35 +0100

    Deleted interaction_BiLSTM

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ def batch_training(dataset, embedding_matrix, batch_size=6, num_epochs=10):
     # Initialize BiLSTMs
     qp_bilstm = BiLSTM(embedding_matrix, embedding_dim=300, hidden_dim=100,
                 batch_size=batch_size)
-    interaction_bilstm = BiLSTM(embedding_matrix, embedding_dim=400, hidden_dim=100,
-                batch_size=batch_size) #check this batch size
     G_bilstm = nn.LSTM(input_size=400, hidden_size=100, bidirectional=True)
 
 
@@ -52,12 +50,14 @@ def batch_training(dataset, embedding_matrix, batch_size=6, num_epochs=10):
         for batch_number, data in enumerate(train_loader):
             questions, contexts, answers, q_len, c_len, a_len, q_id = data
             # Pack (reduce the sentences to their original form without padding)
+            '''
             packed_q = torch.nn.utils.rnn.pack_padded_sequence(questions, q_len, batch_first=True, enforce_sorted=False)
             packed_c = torch.nn.utils.rnn.pack_padded_sequence(contexts, c_len, batch_first=True, enforce_sorted=False)
             packed_a = torch.nn.utils.rnn.pack_padded_sequence(answers, a_len, batch_first=True, enforce_sorted=False)
+            '''
             # Question and Passage Representation
-            q_representation = qp_bilstm.forward(questions)
-            c_representation = qp_bilstm.forward(contexts)
+            q_representation = qp_bilstm.forward(questions, sentence_lengths=q_len)
+            c_representation = qp_bilstm.forward(contexts, sentence_lengths=c_len)
             # Question and Passage Interaction
             HP_attention = attention(q_representation, c_representation)
             G_input = torch.cat((c_representation, HP_attention), 2)
@@ -72,41 +72,44 @@ def batch_training(dataset, embedding_matrix, batch_size=6, num_epochs=10):
             print('scores', scores)
 
 def main(embedding_matrix, encoded_corpora):
-	'''
-	Iterates through all given corpus files and forwards the encoded contexts and questions
-	through the BILSTMs.
-	:param embedding_matrix:
-	:param encoded_corpora:
-	:return:
-	'''
+    '''
+    Iterates through all given corpus files and forwards the encoded contexts and questions
+    through the BILSTMs.
+    :param embedding_matrix:
+    :param encoded_corpora:
+    :return:
+    '''
 
-	embedding_matrix = pickle.load(open(embedding_matrix, 'rb'))
+    embedding_matrix = pickle.load(open(embedding_matrix, 'rb'))
 
-  # Retrieve the filepaths of all encoded corpora
-  file_paths = get_file_paths(encoded_corpora)
+    # Retrieve the filepaths of all encoded corpora
+    file_paths = get_file_paths(encoded_corpora)
 
-  # Apply training to alls sets in the encoded corpora folder
-  for file in file_paths:
-      with open(os.path.join(file), 'rb') as f:
-          content = pickle.load(f)
-          
-          # Minibatch training
-          dataset = qas.Question_Answer_Set(content)
-          batch_training(dataset, embedding_matrix, batch_size=100, num_epochs=10)
+    qp_representations = {}
+    int_representations = {}
 
+    for file in file_paths:
+        with open(os.path.join(file), 'rb') as f:
+            content = pickle.load(f)
+
+            # Minibatch training
+            dataset = qas.Question_Answer_Set(content)
+            batch_training(dataset, embedding_matrix, batch_size=100, num_epochs=10)
 
 if __name__ == '__main__':
+    '''
+    parser = ArgumentParser(
+        description='Main ODQA script')
+    parser.add_argument(
+        'embeddings', help='Path to the pkl file')
+    parser.add_argument(
+        'data', help='Path to the folder with the pkl files')
 
-	parser = ArgumentParser(
-		description='Main ODQA script')
-	parser.add_argument(
-		'embeddings', help='Path to the pkl file')
-	parser.add_argument(
-		'data', help='Path to the folder with the pkl files')
+    # Parse given arguments
+    args = parser.parse_args()
+    '''
 
-	# Parse given arguments
-	args = parser.parse_args()
 
-  # Call main()
-  main(embedding_matrix=args.embeddings, encoded_corpora=args.data)
-  #main(embedding_matrix='embedding_matrix.pkl', encoded_corpora='outputs_numpy_encoding_v2'
+    # Call main()
+    #main(embedding_matrix=args.embeddings, encoded_corpora=args.data)
+    main(embedding_matrix='embedding_matrix.pkl', encoded_corpora='outputs_numpy_encoding_v2')
