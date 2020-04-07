d081d5e5c8cdb3a62034fdbe0b37c0ce071d6693
fgoessl <59576830+fgoessl@users.noreply.github.com>
2020-04-07 11:36:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def batch_training(dataset, embedding_matrix, batch_size=100, num_epochs=10):
             # todo: Do we need to share the weights among the multiple Candidate Rep classes? (Same goes for candidate scores?)
             # In that case we would need to make the Candidate Rep functions take inputs e.g.
             # generate_fused_representation(V). Right now the functions take these values directly from the class.
-            C_rep = Candidate_Represenation(S_p, C_spans, k=K)
+            C_rep = Candidate_Representation(S_p, C_spans, k=K)
             S_Cs = C_rep.S_Cs
             r_Cs = C_rep.r_Cs
             r_Ctilde = C_rep.tilda_r_Cs
