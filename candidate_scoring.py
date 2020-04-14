511f92c39f5a448155cfb1a97b3987eaad96f1cb
“Katya <katjasaveleva@gmail.com>
2020-04-14 20:48:06 +0200

    add bias=False, replace softmax

--- candidate_scoring.py
+++ candidate_scoring.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 
 class Candidate_Scorer():
 	def __init__(self, G_p):
-		self.wb = nn.Linear(200, 1) #1 is the output shape because we want one score as output
-		self.we = nn.Linear(200, 1)
+		self.wb = nn.Linear(200, 1, bias=False) #1 is the output shape because we want one score as output
+		self.we = nn.Linear(200, 1, bias=False)
 		self.G_p = G_p
 
 	def begin_scores(self):
@@ -21,9 +21,11 @@ class Candidate_Scorer():
 		'''
 		b_P = self.begin_scores() #target vector 1x100
 		e_P = self.end_scores() #target vector 1x100
-		candidate_prob = F.softmax(b_P + e_P, dim=0)
+		numerator = torch.exp(b_P + e_P.transpose(0,1))
+		denominator = torch.sum(numerator)
+		candidate_probs = torch.div(numerator, denominator)
 		#get top k candidate indices
-		upper_diagonal = torch.triu(candidate_prob) #set scores in lower triangular matrix to zero
+		upper_diagonal = torch.triu(candidate_probs) #set scores in lower triangular matrix to zero
 		H, W = upper_diagonal.shape
 		flattened = upper_diagonal.view(-1) #flatten to get top k scores of entire tensor
 		k_max_values, flattened_indices = flattened.topk(k)
