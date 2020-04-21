91ce8baff669ab27563eb21ef209b763de6fe420
Fabian Gössl <s8fagoes@stud.uni-saarland.de>
2020-04-21 14:05:20 +0200

    Preparing optimizer/criterion for backprop

--- utils/pretraining.py
+++ utils/pretraining.py
@@ -7,3 +7,10 @@ pretrain_questions = questions[:pretrain_contexts.shape[0],:]
 pretrain_answers = answers[:pretrain_contexts.shape[0],:]
 #then use the pretrain versions for pre-training below
 '''
+
+def pretrain_candidate_scoring(dataset, embedding_matrix, pretrained_parameters_filepath=None, batch_size=100, num_epochs=10):
+    pass
+
+def pretrain_answer_selection(dataset, embedding_matrix, pretrained_parameters_filepath=None, batch_size=100, num_epochs=10):
+    pass
+
