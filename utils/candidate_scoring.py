2e2b449f491156431d716a20128e6e3216233218
fgoessl <59576830+fgoessl@users.noreply.github.com>
2020-05-08 12:56:18 +0200

    Add pretraining parameter

--- utils/candidate_scoring.py
+++ utils/candidate_scoring.py
@@ -8,13 +8,14 @@ class Candidate_Scorer(nn.Module):
 		self.wb = nn.Linear(200, 1, bias=False) #1 is the output shape because we want one score as output
 		self.we = nn.Linear(200, 1, bias=False)
 
+
 	def begin_scores(self, G_p):
 		return self.wb(G_p)
 
 	def end_scores(self, G_p):
 		return self.we(G_p)
 
-	def candidate_probabilities(self, G_p, k):
+	def candidate_probabilities(self, G_p, k, pretraining=False):
 		'''
 		Returns the start and end indices of the top k candidates within a single
 		context.
@@ -22,8 +23,11 @@ class Candidate_Scorer(nn.Module):
 		b_P = self.begin_scores(G_p) #target vector 1x100
 		e_P = self.end_scores(G_p) #target vector 1x100
 		numerator = torch.exp(b_P + e_P.transpose(0,1))
-		denominator = torch.sum(numerator)
-		candidate_probs = torch.div(numerator, denominator)
+		if pretraining:
+			candidate_probs = numerator 
+		else:
+			denominator = torch.sum(numerator)
+			candidate_probs = torch.div(numerator, denominator)
 		#get top k candidate indices
 		upper_diagonal = torch.triu(candidate_probs) #set scores in lower triangular matrix to zero
 		H, W = upper_diagonal.shape
